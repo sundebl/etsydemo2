4fa86d1e4a3bfd3e748a242c53189b674b94d2c4
Brian Sunde <sundebl@msn.com>
2014-05-22 15:03:47 -0500

    first commit

---
+++ app/models/listing.rb
@@ -0,0 +1,5 @@
+class Listing < ActiveRecord::Base
+  has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "default.jpg"
+  do_not_validate_attachment_file_type :image
+
+end
