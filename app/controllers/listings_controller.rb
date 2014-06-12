c1256133cdb1b763feecd7e9782a378458b0adba
Brian Sunde <sundebl@msn.com>
2014-06-12 14:35:42 -0500

    add seller page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,12 +1,16 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_filter :authenticate_user!, only: [:seller, :new, :create, :edit, :update, :destroy]
   before_filter :check_user, only: [:edit, :update, :destroy]
+
+  def seller
+    @listings = Listing.where(user: current_user).order("created_at DESC")
+  end
 
   # GET /listings
   # GET /listings.json
   def index
-    @listings = Listing.all
+    @listings = Listing.all.order("created_at DESC")
   end
 
   # GET /listings/1
@@ -76,7 +80,7 @@ class ListingsController < ApplicationController
     end
 
     def check_user
-      if current_user != @listing.user_id
+      if current_user != @listing.user
         redirect_to root_url, alert: "Sorry, this listing belongs to someone else"
       end
     end
